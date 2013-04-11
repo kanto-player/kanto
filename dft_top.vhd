2c10aaf1513309a7226492257322008863562dec
Howard Mao <zhehao.mao@gmail.com>
2013-04-10 22:05:47 -0400

    port map stages 2 and 3 of dft

--- dft_top.vhd
+++ dft_top.vhd
@@ -24,6 +24,12 @@ architecture rtl of dft_top is
     signal s1_k : unsigned(3 downto 0);
     signal s1_write : std_logic;
     signal s1_done : std_logic;
+
+    signal s2_k : unsigned(3 downto 0);
+    signal s2_write : std_logic;
+    signal s2_done : std_logic;
+    signal s2_res_real : signed(31 downto 0);
+    signal s2_res_imag : signed(31 downto 0);
 begin
     S1 : entity work.dft_stage1 port map (
         tdom_data => tdom_data,
@@ -42,5 +48,41 @@ begin
         outk => s1_k,
         write => s1_write,
         done => s1_done
+    );
+
+    S2 : entity work.dft_stage2 port map (
+        rom_real => s1_rom_real,
+        rom_imag => s1_rom_imag,
+        tdom_real => s1_tdom_real,
+
+        clk => clk,
+
+        res_real => s2_res_real,
+        res_imag => s2_res_imag,
+
+        ink => s1_k,
+        outk => s2_k,
+        inwrite => s1_write,
+        outwrite => s2_write,
+        indone => s1_done,
+        outdone => s2_done
+    );
+
+    S3 : entity work.dft_stage3 port map (
+        mult_real => s2_res_real,
+        mult_imag => s2_res_imag,
+
+        clk => clk,
+        reset => reset,
+
+        indone => s2_done,
+        outdone => done,
+        inwrite => s2_write,
+        outwrite => write,
+        k => s2_k,
+        
+        fdom_addr => fdom_addr,
+        fdom_base => fdom_base,
+        fdom_data => fdom_data
     );
 end rtl;
