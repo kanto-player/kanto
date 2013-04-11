54fb6bd8c0022827863437e828c001c61bab649c
Zhehao Mao <zhehao.mao@gmail.com>
2013-04-11 15:15:26 -0400

    add real_signed_array type

--- types_pkg.vhd
+++ types_pkg.vhd
@@ -1,4 +1,5 @@
 package types_pkg is
     type complex_signed_array is array (0 to 15) of signed(35 downto 0);
+    type real_signed_array is array(0 to 15) of signed(17 downto 0);
     type byte_array is array(0 to 15) of unsigned(7 downto 0);
 end package;
