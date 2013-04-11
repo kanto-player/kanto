131a090cf2d3d066833a7a5947b4758d2e633a1c
Zhehao Mao <zhehao.mao@gmail.com>
2013-04-10 20:42:17 -0400

    add dft stages 1 and 2

--- dft_stage2.vhd
+++ dft_stage2.vhd
@@ -7,6 +7,8 @@ entity dft_stage2 is
           rom_imag : in signed(17 downto 0);
           tdom_real : in signed(15 downto 0);
 
+          clk : std_logic;
+
           res_real : out signed(31 downto 0);
           res_imag : out signed(31 downto 0);
 
@@ -21,19 +23,39 @@ entity dft_stage2 is
 end dft_stage2;
 
 architecture rtl of dft_stage2 is
-    signal mult_real : out signed(35 downto 0);
-    signal mult_imag : out signed(35 downto 0);
-    signal copy_bit : std_logic;
+    signal mult_real : signed(35 downto 0);
+    signal mult_imag : signed(35 downto 0);
+    signal tdom_extended : signed(17 downto 0);
+    signal input_copy_bit : std_logic;
+    signal real_copy_bit : std_logic;
+    signal imag_copy_bit : std_logic;
 begin
+    input_copy_bit <= tdom_real(15);
+    real_copy_bit <= mult_real(33);
+    imag_copy_bit <= mult_imag(33);
+
+    tdom_extended <= (1 downto 0 => input_copy_bit) & tdom_real;
+
     REALM : entity work.mult port map (
         dataa => rom_real,
-        datab => tdom_real,
+        datab => tdom_extended,
         result => mult_real
     );
 
     IMAGM : entity work.mult port map (
-        dataa => rom_real,
-        datab => rom_imag,
+        dataa => rom_imag,
+        datab => tdom_extended,
         result => mult_imag
     );
+
+    process (clk)
+    begin
+        if rising_edge(clk) then
+            outwrite <= inwrite;
+            outdone <= indone;
+            outk <= ink;
+            res_real <= (3 downto 0 => real_copy_bit) & mult_real(33 downto 6);
+            res_imag <= (3 downto 0 => imag_copy_bit) & mult_imag(33 downto 6);
+        end if;
+    end process;
 end rtl;
---
+++ dft_stage3.vhd
@@ -0,0 +1,51 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity dft_stage3 is
+    port (mult_real : in signed(31 downto 0);
+          mult_imag : in signed(31 downto 0);
+
+          clk : in std_logic;
+          reset : in std_logic;
+
+          indone : in std_logic;
+          inwrite : in std_logic;
+          k : in signed(3 downto 0);
+          
+          outdone : out std_logic;
+          outwrite : out std_logic;
+
+          fdom_data : out signed(35 downto 0);
+          fdom_addr : out unsigned(3 downto 0));
+end dft_stage3;
+
+architecture rtl of dft_stage3 is
+    signal sum_real : signed(31 downto 0);
+    signal sum_imag : signed(31 downto 0);
+begin
+    process (clk)
+    begin
+        if rising_edge(clk) then
+            outdone <= indone;
+            outwrite <= inwrite;
+            if reset = '1' then
+                sum_real <= (others => '0');
+                sum_imag <= (others => '0');
+                outdone <= '0';
+                outwrite <= '0';
+                fdom_data <= (others => '0');
+                fdom_addr <= (others => '0');
+            if inwrite = '1' then
+                fdom_addr <= k;
+                fdom_data <= sum_real(31 downto 14) & sum_imag(31 downto 14);
+                sum_real <= mult_real;
+                sum_imag <= sum_imag;
+            else
+                sum_real <= sum_real + mult_real;
+                sum_imag <= sum_imag + mult_imag;
+                outwrite <= '1';
+            end if;
+        end if;
+    end process;
+end rtl;
