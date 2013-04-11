240a81c70b6e54096e945e7ffa439f36d5019b3b
Zhehao Mao <zhehao.mao@gmail.com>
2013-04-10 20:45:35 -0400

    pull stage1 into dft_top

--- dft_top.vhd
+++ dft_top.vhd
@@ -15,5 +15,28 @@ entity dft_top is
 end dft_top;
 
 architecture rtl of dft_top is
+    signal s1_rom_real : signed(17 downto 0);
+    signal s1_rom_imag : signed(17 downto 0);
+    signal s1_tdom_real : signed(15 downto 0);
+    signal s1_k : unsigned(3 downto 0);
+    signal s1_write : std_logic;
+    signal s1_done : std_logic;
 begin
+    S1 : entity work.dft_stage1 port map (
+        tdom_data => tdom_data,
+        tdom_addr => tdom_addr,
+
+        clk => clk,
+        reset => reset,
+
+        rom_data => rom_data,
+        rom_addr => rom_addr,
+
+        rom_real => s1_rom_real,
+        rom_imag => s1_rom_imag,
+        tdom_real => s1_tdom_real,
+        outk => s1_k,
+        write => s1_write,
+        done => s1_done
+    );
 end rtl;
