bd0ca193a55d3d7eecd44c91d3ec62a5904757d0
Zhehao Mao <zhehao.mao@gmail.com>
2013-04-11 15:15:00 -0400

    make two rams for fft

--- fft_fdom_ram.vhd
+++ fft_fdom_ram.vhd
@@ -5,27 +5,24 @@ use ieee.numeric_std.all;
 library work;
 use work.types_pkg.all;
 
-entity fft_mid_ram is
+entity fft_fdom_ram is
     port (writedata : in complex_signed_array;
           readdata : out complex_signed_array;
           addr : in byte_array;
           write_en : in std_logic_vector(15 downto 0);
           clk : std_logic);
-end fft_mid_ram;
+end fft_fdom_ram;
 
-architecture rtl of fft_mid_ram is
+architecture rtl of fft_fdom_ram is
     type ram_type is array(0 to 255) of signed(35 downto 0);
     signal ram_data : ram_type;
 begin
     LUMAP : for i in 0 to 15 generate
+        readdata <= ram(to_integer(addr(i)));
         process (clk)
         begin
-            if rising_edge(clk) then
-                if write_en(i) = '1' then
-                    ram_data(to_integer(addr(i))) <= writedata(i);
-                else
-                    readdata <= ram(to_integer(addr(i)));
-                end if;
+            if rising_edge(clk) and write_en(i) = '1' then
+                ram_data(to_integer(addr(i))) <= writedata(i);
             end if;
         end process;
     end generate LUMAP;
---
+++ fft_tdom_ram.vhd
@@ -0,0 +1,29 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+use work.types_pkg.all;
+
+entity fft_tdom_ram is
+    port (writedata : in real_signed_array;
+          readdata : out real_signed_array;
+          addr : in byte_array;
+          write_en : in std_logic_vector(15 downto 0);
+          clk : std_logic);
+end fft_tdom_ram;
+
+architecture rtl of fft_tdom_ram is
+    type ram_type is array(0 to 255) of signed(17 downto 0);
+    signal ram_data : ram_type;
+begin
+    LUMAP : for i in 0 to 15 generate
+        readdata <= ram(to_integer(addr(i)));
+        process (clk)
+        begin
+            if rising_edge(clk) and write_en(i) = '1' then
+                ram_data(to_integer(addr(i))) <= writedata(i);
+            end if;
+        end process;
+    end generate LUMAP;
+end rtl;
