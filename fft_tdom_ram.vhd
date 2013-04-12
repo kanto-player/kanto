f06db27e99d00b3c507199fc62be41b857679c3d
Howard Mao <zhehao.mao@gmail.com>
2013-04-11 23:30:38 -0400

    tdom_ram should have writes separate from reads

--- fft_tdom_ram.vhd
+++ fft_tdom_ram.vhd
@@ -6,10 +6,11 @@ library work;
 use work.types_pkg.all;
 
 entity fft_tdom_ram is
-    port (writedata : in real_signed_array;
+    port (writedata : in signed(15 downto 0);
+          writeaddr : in unsigned(7 downto 0);
+          write_en : in std_logic;
           readdata : out real_signed_array;
-          addr : in byte_array;
-          write_en : in std_logic_vector(15 downto 0);
+          readaddr : in byte_array;
           clk : std_logic);
 end fft_tdom_ram;
 
@@ -18,12 +19,13 @@ architecture rtl of fft_tdom_ram is
     signal ram_data : ram_type;
 begin
     LUMAP : for i in 0 to 15 generate
-        readdata <= ram(to_integer(addr(i)));
-        process (clk)
-        begin
-            if rising_edge(clk) and write_en(i) = '1' then
-                ram_data(to_integer(addr(i))) <= writedata(i);
-            end if;
-        end process;
+        readdata <= ram(to_integer(readaddr(i)));
     end generate LUMAP;
+
+    process (clk)
+    begin
+        if rising_edge(clk) and write_en = '1' then
+            ram(to_integer(writeaddr)) <= writedata;
+        end if;
+    end process;
 end rtl;
