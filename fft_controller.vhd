81689820d54f968cd83437d46c364247bec0f35a
Howard Mao <zhehao.mao@gmail.com>
2013-04-13 17:15:21 -0400

    hook middleman into fft_controller

--- fft_controller.vhd
+++ fft_controller.vhd
@@ -12,6 +12,7 @@ entity fft_controller is
           sram_write : out std_logic;
           sram_req : out std_logic;
           sram_ack : in std_logic;
+          sram_base : in unsigned(9 downto 0);
 
           clk : in std_logic;
           start : in std_logic);
@@ -29,7 +30,9 @@ architecture rtl of fft_controller is
     signal tdom_readaddr : byte_array;
     signal fdom_writedata : complex_signed_array;
     signal fdom_readdata : complex_signed_array;
-    signal fdom_addr : nibble_array;
+    signal fdom_rwaddr : nibble_array;
+    signal fdom_bigdata : signed(31 downto 0);
+    signal fdom_bigaddr : unsigned(7 downto 0);
     signal fdom_write_en : std_logic_vector(0 to 15);
     signal dft_rom_data : complex_signed_array;
     signal dft_rom_addr : byte_array;
@@ -37,6 +40,9 @@ architecture rtl of fft_controller is
     signal dft_out_addr : nibble_array;
     signal dft_out_write : std_logic_vector(0 to 15);
     signal dft_done : std_logic_vector(0 to 15);
+    signal dft_reset : std_logic;
+    signal mm_done : std_logic;
+    signal start_write : std_logic;
     type reorder_type is array(0 to 15) of unsigned(3 downto 0); 
     constant fft_reorder : reorder_type := (x"0", x"8", x"4", x"c", 
                                             x"2", x"a", x"6", x"e", 
@@ -55,7 +61,9 @@ begin
     FDOM_RAM : entity work.fft_fdom_ram port map (
         writedata => fdom_writedata,
         readdata => fdom_readdata,
-        addr => fdom_addr,
+        bigdata => fdom_bigdata,
+        bigaddr => fdom_bigaddr,
+        rwaddr => fdom_rwaddr,
         write_en => fdom_write_en,
         clk => clk
     );
@@ -72,7 +80,7 @@ begin
             tdom_offset => fft_reorder(i),
 
             clk => clk,
-            reset => start,
+            reset => dft_reset,
 
             rom_data => dft_rom_data(i),
             rom_addr => dft_rom_addr(i),
@@ -87,13 +95,35 @@ begin
         with control_state select fdom_writedata(i) <=
             dft_out_data(i) when dftcomp,
             (others => '0') when others;
-        with control_state select fdom_addr(i) <=
+        with control_state select fdom_rwaddr(i) <=
             dft_out_addr(i) when dftcomp,
             (others => '0') when others;
         with control_state select fdom_write_en(i) <=
             dft_out_write(i) when dftcomp,
             '0' when others;
     end generate DFT_GEN;
+
+    MM : entity work.fft_middleman port map (
+        clk => clk,
+        done => mm_done,
+        start_read => start,
+        start_write => start_write,
+        
+        sram_req => sram_req,
+        sram_ack => sram_ack,
+        sram_write => sram_write,
+        sram_readdata => sram_readdata,
+        sram_writedata => sram_writedata,
+        sram_addr => sram_addr,
+
+        tdom_data => tdom_writedata,
+        tdom_addr => tdom_writeaddr,
+        tdom_write => tdom_write_en,
+        tdom_base => sram_base,
+
+        fdom_data => fdom_bigdata,
+        fdom_addr => fdom_bigaddr
+    );
 
     process (clk)
     begin
@@ -103,7 +133,13 @@ begin
                     if start = '1' then
                         control_state <= loading;
                     end if;
+                when loading =>
+                    if mm_done = '1' then
+                        control_state <= dftcomp;
+                        dft_reset <= '1';
+                    end if;
                 when dftcomp =>
+                    dft_reset <= '0';
                     if dft_done = x"ffff" then
                         control_state <= recomb1;
                     end if;
