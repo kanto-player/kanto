65e150862f8e398f4985c140287de28e2618a959
Darien <don2102@micro8.ilab.columbia.edu>
2013-04-14 13:41:46 -0400

    sram cntrlr finished, sram tb in progress

--- sram_controller.vhd
+++ sram_controller.vhd
@@ -6,7 +6,13 @@ entity sram_controller is
 	port(
 		clk, reset : in std_logic;
 		
-		--sd
+	  SRAM_ADDR_out : OUT STD_LOGIC_VECTOR (17 DOWNTO 0);
+	  SRAM_CE_N_out : OUT STD_LOGIC;
+	  SRAM_DQ_inout : INOUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+	  SRAM_OE_N_out : OUT STD_LOGIC;
+	  SRAM_WE_N_out : OUT STD_LOGIC;
+
+	  --sd
 		sd_readdata 	: out std_logic_vector(15 downto 0);
 		sd_writedata 	: in std_logic_vector(15 downto 0);
 		sd_addr 			: in std_logic_vector(17 downto 0);
@@ -42,14 +48,16 @@ end sram_controller;
 
 architecture moore of sram_controller is
 	type states is (ZERO, 
-						SD_1, SD_2,
-						AB_1, AB_2,
-						FFT_1, FFT_2,
-						VIZ_1, VIZ_2);
+						SD_1, SD_2, SD_3,
+						AB_1, AB_2, AB_3,
+						FFT_1, FFT_2, FFT_3,
+						VIZ_1, VIZ_2, VIZ_3);
 begin
 	process (clk)
 	variable state : states;
 	begin
+		SRAM_CE_N_out <= '0';
+		SRAM_OE_N_out <= '0';
 		if rising_edge(clk) then
 			if reset = '1' then state := ZERO;
 			else case state is
@@ -64,61 +72,82 @@ begin
 				-- SD REQ AREA --	
 				when SD_1 =>
 					sd_ack <= '1';
-					if sd_write = '1' then sram(sd_addr) <= sd_writedata;
-					else sd_readdata <= sram(sd_addr);
-					end if;
+					SRAM_WE_N_out <= NOT sd_write;
+					SRAM_ADDR_out <= sd_addr;
 					state := SD_2;
 					
-				when SD_2 =>  -- waiting for sd_req to go low --
+				when SD_2 => -- push or pull data from dq line
+					if sd_write = '1' then SRAM_DQ_inout <= sd_writedata;
+					else sd_readdata <= SRAM_DQ_inout;
+					end if;
+					state := SD_3;
+					
+				when SD_3 =>  -- waiting for sd_req to go low --
 					if sd_req = '0' then
 						sd_ack <= '0';
 						state := ZERO;
-					else state := SD_2;
+					else state := SD_3;
 					end if;
 					
 				-- AB REQ AREA --	
 				when AB_1 =>
 					ab_ack <= '1';
-					if ab_write = '1' then sram(ab_addr) <= ab_writedata;
-					else ab_readdata <= sram(ab_addr);
-					end if;
+					SRAM_WE_N_out <= NOT ab_write;
+					SRAM_ADDR_out <= ab_addr;
 					state := AB_2;
 					
-				when AB_2 =>  -- waiting for ab_req to go low --
+				when AB_2 => -- push or pull data from dq line
+					if ab_write = '1' then SRAM_DQ_inout <= ab_writedata;
+					else ab_readdata <= SRAM_DQ_inout;
+					end if;
+					state := AB_3;
+					
+				when AB_3 =>  -- waiting for ab_req to go low --
 					if ab_req = '0' then
 						ab_ack <= '0';
 						state := ZERO;
-					else state := AB_2;
-					end if;	
-		
+					else state := AB_3;
+					end if;
+							
 				-- FFT REQ AREA --	
 				when FFT_1 =>
 					fft_ack <= '1';
-					if fft_write = '1' then sram(fft_addr) <= fft_writedata;
-					else fft_readdata <= sram(fft_addr);
-					end if;
+					SRAM_WE_N_out <= NOT fft_write;
+					SRAM_ADDR_out <= fft_addr;
 					state := FFT_2;
 					
-				when FFT_2 =>  -- waiting for fft_req to go low --
+				when FFT_2 => -- push or pull data from dq line
+					if fft_write = '1' then SRAM_DQ_inout <= fft_writedata;
+					else fft_readdata <= SRAM_DQ_inout;
+					end if;
+					state := FFT_3;
+
+					
+				when FFT_3 =>  -- waiting for fft_req to go low --
 					if fft_req = '0' then
 						fft_ack <= '0';
 						state := ZERO;
-					else state := FFT_2;
+					else state := FFT_3;
 					end if;
 					
 				-- VIZ REQ AREA --	
 				when VIZ_1 =>
 					viz_ack <= '1';
-					if viz_write = '1' then sram(viz_addr) <= viz_writedata;
-					else viz_readdata <= sram(viz_addr);
-					end if;
+					SRAM_WE_N_out <= NOT viz_write;
+					SRAM_ADDR_out <= viz_addr;
 					state := VIZ_2;
 					
-				when VIZ_2 =>  -- waiting for sd_req to go low --
+				when VIZ_2 => -- push or pull data from dq line
+					if viz_write = '1' then SRAM_DQ_inout <= viz_writedata;
+					else viz_readdata <= SRAM_DQ_inout;
+					end if;
+					state := VIZ_3;
+					
+				when VIZ_3 =>  -- waiting for viz_req to go low --
 					if viz_req = '0' then
 						viz_ack <= '0';
 						state := ZERO;
-					else state := VIZ_2;
+					else state := VIZ_3;
 					end if;	
 
 		
