f21b859631be5b3a3a49e7cea823b7e8cb7bb8bd
Kavita Jain-Cocks <kj2264@columbia.edu>
2013-04-19 12:05:10 -0400

    visualizer compiling, next add portmap

--- visualizer.vhd
+++ visualizer.vhd
@@ -13,8 +13,9 @@ use ieee.numeric_std.all;
 entity visualizer is
   
   port (
-    reset : in std_logic;
+    --reset : in std_logic;
     clk   : in std_logic;                    -- Should be 25.125 MHz
+	 reset_data: in std_logic;
 	 
 	 --viz_readdata  : in std_logic_vector(15 downto 0);
 	 --viz_writedata : out std_logic_vector(15 downto 0);
@@ -56,7 +57,7 @@ architecture rtl of visualizer is
   
   constant bar_w : integer := 40;
   
-  type states is (A,B,C,D);
+  type states is (A,B);
 
   
   
@@ -75,9 +76,9 @@ architecture rtl of visualizer is
 
   signal rectangle : std_logic;  -- rectangle area
  
-  type sums is array (0 to 15) of std_logic_vector(19 downto 0):"00000000000000000000";
+  type ram_type is array (0 to 15) of std_logic_vector(19 downto 0);
   
-  signal sum : sums;
+  signal sum : ram_type := ((others=>(others =>'0')));
   
   signal read_complete  : std_logic := '0';
   signal address_r      : integer := 512;
@@ -86,7 +87,7 @@ architecture rtl of visualizer is
   signal counter 			: integer := 0;
   signal addr_counter   : integer := 0;
   signal sum_counter    : integer := 0;	
-  signal display        : std_logic := '0'; 
+  signal reset        : std_logic := '0'; 
 begin
 
   -- Horizontal and vertical counters
@@ -98,76 +99,58 @@ begin
 		if read_complete='0' then
 		case state is
 			when A =>
-				if reset = '1' then
-					display <= '0';
+				if reset_data= '1' then
+					reset <= '0';
 					state := B;
 				else 
 					state:= A;
 				end if;
 			when B => 
 				if addr_counter = 0 then
-					fft_fdom_addr <= addr_counter;
+					fft_fdom_addr <= to_unsigned(addr_counter,fft_fdom_addr'length);
 					addr_counter  <= addr_counter + 1;
-				elsif fft_fdom_addr < 256 then
+				elsif addr_counter < 256 then
 					if addr_counter <= 16 then 
-						sum(0) <= sum(0) + fft_fdom_data;
+						sum(0) <= std_logic_vector(unsigned(sum(0)) + unsigned(fft_fdom_data));
 					elsif addr_counter <= 32 then 
-						sum(1) <= sum(1) + fft_fdom_data;
+						sum(1) <= std_logic_vector(unsigned(sum(1)) + unsigned(fft_fdom_data));
 					elsif addr_counter <= 48 then 
-						sum(2) <= sum(2) + fft_fdom_data;
+						sum(2) <= std_logic_vector(unsigned(sum(2)) + unsigned(fft_fdom_data));
 					elsif addr_counter <= 64 then 
-						sum(3) <= sum(3) + fft_fdom_data;
+						sum(3) <= std_logic_vector(unsigned(sum(3)) + unsigned(fft_fdom_data));
 					elsif addr_counter <= 80 then 
-						sum(4) <= sum(4) + fft_fdom_data;
+						sum(4) <= std_logic_vector(unsigned(sum(4)) + unsigned(fft_fdom_data));
 					elsif addr_counter <= 96 then 
-						sum(5) <= sum(5) + fft_fdom_data;
+						sum(5) <= std_logic_vector(unsigned(sum(5)) + unsigned(fft_fdom_data));
 					elsif addr_counter <= 112 then 
-						sum(6) <= sum(6) + fft_fdom_data;
+						sum(6) <= std_logic_vector(unsigned(sum(6)) + unsigned(fft_fdom_data));
 					elsif addr_counter <= 128 then 
-						sum(7) <= sum(7) + fft_fdom_data;
+						sum(7) <= std_logic_vector(unsigned(sum(7)) + unsigned(fft_fdom_data));
 					elsif addr_counter <= 144 then 
-						sum(8) <= sum(8) + fft_fdom_data;
+						sum(8) <= std_logic_vector(unsigned(sum(8)) + unsigned(fft_fdom_data));
 					elsif addr_counter <= 160 then 
-						sum(9) <= sum(9) + fft_fdom_data;	
+						sum(9) <= std_logic_vector(unsigned(sum(9)) + unsigned(fft_fdom_data));
 					elsif addr_counter <= 176 then 
-						sum(10) <= sum(10) + fft_fdom_data;
+						sum(10) <= std_logic_vector(unsigned(sum(10)) + unsigned(fft_fdom_data));
 					elsif addr_counter <= 192 then 
-						sum(11) <= sum(11) + fft_fdom_data;
+						sum(11) <= std_logic_vector(unsigned(sum(11)) + unsigned(fft_fdom_data));
 					elsif addr_counter <= 208 then 
-						sum(12) <= sum(12) + fft_fdom_data;
+						sum(12) <= std_logic_vector(unsigned(sum(12)) + unsigned(fft_fdom_data));
 					elsif addr_counter <= 224 then 
-						sum(13) <= sum(13) + fft_fdom_data;
+						sum(13) <= std_logic_vector(unsigned(sum(13)) + unsigned(fft_fdom_data));
 					elsif addr_counter <= 240 then 
-						sum(14) <= sum(14) + fft_fdom_data;
+						sum(14) <= std_logic_vector(unsigned(sum(14)) + unsigned(fft_fdom_data));
 					else
-						sum(15) <= sum(15) + fft_fdom_data;
+						sum(15) <= std_logic_vector(unsigned(sum(15)) + unsigned(fft_fdom_data));
 					end if;
-					fft_fdom_addr <= addr_counter;
+					fft_fdom_addr <= to_unsigned(addr_counter,fft_fdom_addr'length);
 					addr_counter  <= addr_counter + 1;
 				else
-					addr_counter <= '0';
-					sum(15) <= sum(15) + fft_fdom_data;
-					state := C;
-				end if;std_logic
-			when C =>
-				sum(0)=sum(0) srl 4;
-				sum(1)=sum(1) srl 4;
-				sum(2)=sum(2) srl 4;
-				sum(3)=sum(3) srl 4;
-				sum(4)=sum(4) srl 4;
-				sum(5)=sum(5) srl 4;
-				sum(6)=sum(6) srl 4;
-				sum(7)=sum(7) srl 4;
-				sum(8)=sum(8) srl 4;
-				sum(9)=sum(9) srl 4;
-				sum(10)=sum(10) srl 4;
-				sum(11)=sum(11) srl 4;
-				sum(12)=sum(12) srl 4;
-				sum(13)=sum(13) srl 4;
-				sum(14)=sum(14) srl 4;
-				sum(15)=sum(15) srl 4;
-				display <= '1';
-				state := A;
+					addr_counter <= 0;
+					sum(15) <= std_logic_vector(unsigned(sum(15)) + unsigned(fft_fdom_data));
+					reset <= '1';
+					state := A;
+				end if;
 			end case;
 		end if;
 	end if;
@@ -312,97 +295,97 @@ begin
 			rectangle<='0';
 		--division 1
 		elsif Hcount >= HSYNC+HBACK_PORCH AND Hcount<=HSYNC+HBACK_PORCH+bar_w then
-			if Vcount > VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base)(15 downto 8))) then
+			if Vcount > VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(0)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 2
 		elsif Hcount>=HSYNC+HBACK_PORCH+bar_w AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*2) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+16)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(1)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 3
 		elsif Hcount>=HSYNC+HBACK_PORCH+(bar_w*2) AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*3) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+32)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(2)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 4
 		elsif Hcount>=HSYNC+HBACK_PORCH+(bar_w*3) AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*4) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+48)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(3)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 5
 		elsif Hcount>=HSYNC+HBACK_PORCH+(bar_w*4) AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*5) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+64)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(4)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 6
 		elsif Hcount>=HSYNC+HBACK_PORCH+(bar_w*5) AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*6) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+80)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(5)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 7
 		elsif Hcount>=HSYNC+HBACK_PORCH+(bar_w*6) AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*7) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+96)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(6)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 8
 		elsif Hcount>=HSYNC+HBACK_PORCH+(bar_w*7) AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*8) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+112)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(7)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 9
 		elsif Hcount>=HSYNC+HBACK_PORCH+(bar_w*8) AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*9) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+128)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(8)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 10
 		elsif Hcount>=HSYNC+HBACK_PORCH+(bar_w*9) AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*10) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+144)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(9)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 11
 		elsif Hcount>=HSYNC+HBACK_PORCH+(bar_w*10) AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*11) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+160)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(10)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 12
 		elsif Hcount>=HSYNC+HBACK_PORCH+(bar_w*11) AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*12) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+176)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(11)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 13
 		elsif Hcount>=HSYNC+HBACK_PORCH+(bar_w*12) AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*13) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+192)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(12)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 14
 		elsif Hcount>=HSYNC+HBACK_PORCH+(bar_w*13) AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*14) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+208)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(13)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 15
 		elsif Hcount>=HSYNC+HBACK_PORCH+(bar_w*14) AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*15) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+224)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(14)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
 		--division 16
 		elsif Hcount>=HSYNC+HBACK_PORCH+(bar_w*15) AND Hcount<=HSYNC+HBACK_PORCH+(bar_w*16) then
-			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(ram_data(sram_base+240)(15 downto 8))) then
+			if Vcount>VTOTAL-VFRONT_PORCH-to_integer(unsigned(sum(15)(19 downto 12))) then
 				rectangle<='1';
 			else rectangle <='0';
 			end if;
