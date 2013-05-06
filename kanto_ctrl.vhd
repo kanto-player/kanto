1330ed21a4a2f1342a05350e41965d2bc522a9ed
Howard Mao <zhehao.mao@gmail.com>
2013-05-06 17:16:33 -0400

    put reset_n on clock interface

--- kanto_ctrl.vhd
+++ kanto_ctrl.vhd
@@ -10,13 +10,13 @@ use IEEE.numeric_std.all;
 entity kanto_ctrl is
 	port (
 		clk            : in  std_logic                     := '0';             --          clock.clk
-		reset_n        : in  std_logic                     := '0';             --          reset.reset_n
-		read           : in  std_logic                     := '0';             -- avalon_slave_0.read
-		write          : in  std_logic                     := '0';             --               .write
+		reset_n        : in  std_logic                     := '0';             --               .reset_n
+		write          : in  std_logic                     := '0';             -- avalon_slave_0.write
 		chipselect     : in  std_logic                     := '0';             --               .chipselect
 		address        : in  std_logic_vector(2 downto 0)  := (others => '0'); --               .address
 		readdata       : out std_logic_vector(31 downto 0);                    --               .readdata
 		writedata      : in  std_logic_vector(31 downto 0) := (others => '0'); --               .writedata
+		read           : in  std_logic                     := '0';             --               .read
 		nios_readblock : out std_logic;                                        --    conduit_end.export
 		nios_play      : out std_logic;                                        --               .export
 		nios_stop      : out std_logic;                                        --               .export
@@ -31,12 +31,12 @@ architecture rtl of kanto_ctrl is
 		port (
 			clk            : in  std_logic                     := 'X';             -- clk
 			reset_n        : in  std_logic                     := 'X';             -- reset_n
-			read           : in  std_logic                     := 'X';             -- read
 			write          : in  std_logic                     := 'X';             -- write
 			chipselect     : in  std_logic                     := 'X';             -- chipselect
 			address        : in  std_logic_vector(2 downto 0)  := (others => 'X'); -- address
 			readdata       : out std_logic_vector(31 downto 0);                    -- readdata
 			writedata      : in  std_logic_vector(31 downto 0) := (others => 'X'); -- writedata
+			read           : in  std_logic                     := 'X';             -- read
 			nios_readblock : out std_logic;                                        -- export
 			nios_play      : out std_logic;                                        -- export
 			nios_stop      : out std_logic;                                        -- export
@@ -51,13 +51,13 @@ begin
 	kanto_ctrl : component de2_kanto_ctrl
 		port map (
 			clk            => clk,            --          clock.clk
-			reset_n        => reset_n,        --          reset.reset_n
-			read           => read,           -- avalon_slave_0.read
-			write          => write,          --               .write
+			reset_n        => reset_n,        --               .reset_n
+			write          => write,          -- avalon_slave_0.write
 			chipselect     => chipselect,     --               .chipselect
 			address        => address,        --               .address
 			readdata       => readdata,       --               .readdata
 			writedata      => writedata,      --               .writedata
+			read           => read,           --               .read
 			nios_readblock => nios_readblock, --    conduit_end.export
 			nios_play      => nios_play,      --               .export
 			nios_stop      => nios_stop,      --               .export
